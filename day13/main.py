dbd15e8a4196dd6f9f6dcc75c8791ba0cfff4d23
Linda Li <linda@vulcancollective.io>
2024-12-16 22:07:51 -0700

    do some math

--- day13/main.py
+++ day13/main.py
@@ -12,56 +12,20 @@ def getNumTokens():
 def getNumTokensWithConversionError():
   tokens = 0
   for data in getClawDataWithConversionError():
-    print(data)
     tokens += getTokens(data)
   return tokens
 
-def getTokens(data):
+def getTokens(data):  
   a,b,p = KEYS['A'], KEYS['B'], KEYS['P']
   if a not in data or b not in data or p not in data: 
     print('bad input!')
     return 0
   (aX, aY), (bX,bY), (pX,pY) = data[a], data[b], data[p]
-  gX = gcd(aX,bX); gY = gcd(aY,bY)
-  if pX%gX != 0 or pY%gY != 0: return 0
-
-  # Solve each equation
-  x1, y1, a1, b1 = solve_linear_diophantine(aX, bX, pX)
-  x2, y2, a2, b2 = solve_linear_diophantine(aY, bY, pY)
-
-  # Generate solutions to satisfy both equations
-  for k1 in range(-10, 10):  # Adjust range based on problem size
-    x = x1 + k1 * b1
-    y = y1 - k1 * a1
-
-    # Check if this satisfies the second equation
-    if 66 * x + 21 * y == 10000000012176:
-      # Minimize 3x + y
-      cost = 3 * x + y
-      print(f"x = {x}, y = {y}, cost = {cost}")
-      return cost
-
-  tokenPossibilities = []
-  print(getPossibilities(pX,aX,bX,gX))
-  for (aCount,bCount) in getPossibilities(pX,aX,bX,gX):
-    if aX*aCount+bX*bCount == pX and aY*aCount+bY*bCount == pY:
-        tokenPossibilities.append(aCount*3 + bCount)
-
-  print('token pos: ', tokenPossibilities)
-  return min(tokenPossibilities, default=0)
-
-# Solve single equation ax + by = c
-def solve_linear_diophantine(a, b, c):
-  g, x0, y0 = egcd(a, b)
-  if c % g != 0: return None  # No solution
-  scale = c // g
-  return x0 * scale, y0 * scale, a // g, b // g
-
-def egcd(a, b):
-  if b == 0:
-      return a, 1, 0
-  g, x1, y1 = egcd(b, a % b)
-  return g, y1, x1 - (a // b) * y1
+  bCount = (aX*pY-aY*pX)//(bY*aX-bX*aY)
+  aCount = (pX-bX*bCount)//aX
+  if aX*aCount+bX*bCount == pX and aY*aCount+bY*bCount == pY:
+    return aCount*3 + bCount
+  return 0
 
 def getTokensMax100(data):
   a,b,p = KEYS['A'], KEYS['B'], KEYS['P']
@@ -78,7 +42,7 @@ def getTokensMax100(data):
 
 def getClawDataWithConversionError():
   data = []
-  with open('day13/inputtest.txt', 'r') as file:
+  with open('day13/input.txt', 'r') as file:
     res = {}
     for line in file:
       if line.strip():
@@ -95,6 +59,7 @@ def getClawDataWithConversionError():
       else: 
         data.append(res)
         res = {}
+  if res not in data and len(res)>0: data.append(res)
   return data
 
 def getClawData():
@@ -117,7 +82,6 @@ def getClawData():
 
 def main():
   # print(getNumTokens()) #36571
-  # print(getNumTokensWithConversionError())
-  print(find_min_cost())
+  print(getNumTokensWithConversionError()) # 85527711500010
 
 main()
